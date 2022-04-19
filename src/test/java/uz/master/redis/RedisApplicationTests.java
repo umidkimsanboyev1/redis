6576d659fbb665f813a4f002467445d4a547dbb1
umidkimsanboyev1 <umidmaster@yandex.com>
2022-04-19 15:35:37 +0500

    initi

---
+++ src/test/java/uz/master/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.master.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
